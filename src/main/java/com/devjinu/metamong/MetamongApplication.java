7414751982a9da430ebe61f3fe4b64227d1cc7d0
kaishail <kaishail0307@gmail.com>
2021-02-02 00:33:22 +0900

    spring boot init

---
+++ src/main/java/com/devjinu/metamong/MetamongApplication.java
@@ -0,0 +1,13 @@
+package com.devjinu.metamong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetamongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetamongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devjinu/metamong/MetamongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjinu.metamong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetamongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
